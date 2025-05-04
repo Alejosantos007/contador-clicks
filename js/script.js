91769c1516e7ca549b4267a67a48412628fbeea4
Alejo Santos <Alejosantos.2024@gmail.com>
2025-05-04 21:24:55 +0000

    Codigos agregados

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+const countDisplay = document.getElementById("count");
+const clickBtn = document.getElementById("clickBtn");
+
+clickBtn.addEventListener("click", () => {
+  count++;
+  countDisplay.textContent = count;
+});
