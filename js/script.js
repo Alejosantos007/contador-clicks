decf7b80f20c9b64b0a593ba0d934c0e6118507b
AlejoSantos007 <alejosantos.2024@gmail.com>
2025-05-04 19:06:19 -0300

    Se ha agregado la funcion para que el boton de disminuir funcione junto a su id en el boton del html

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ let count = 0;
 
 const countDisplay = document.getElementById("count");
 const clickBtn = document.getElementById("clickBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
 
 clickBtn.addEventListener("click", () => {
   count++;
   countDisplay.textContent = count;
 });
+
+decreaseBtn.addEventListener("click", () =>{
+  if (count > 0) {
+  count--;
+  countDisplay.textContent = count;
+}});
